ae01c416b8496f4409c271e39bfe23f85570a21c
Naofumi Shiota <2311140037x@ed.fuk.kindai.ac.jp>
2025-10-08 08:58:51 +0000

    kiso2

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,19 +1,27 @@
 class TopController < ApplicationController
-    def main
-        if session[:login_uid] != nil
-            render "main"
-        else
-            render "login"
-        end
+  def main
+    if session[:login_uid].present?
+      render "main"  
+    else
+      render "login"
+    end 
+  end
+  
+
+
+  def login
+    user = User.find_by(uid: params[:uid])
+    if user && BCrypt::Password.new(user.pass) == params[:pass]
+      session[:login_uid] = user.uid
+      redirect_to top_main_path
+    else
+      render "error", status: 422
     end
-    
-    def login
-        if params[:uid] == "kindai" && params[:pass] == "sanriko"
-            session[:uid] = params[:uid]
-            redirect_to top_main_path
-        else
-            render "error",status: 422
-        end
-        
-    end
+  end
+
+
+  def logout
+    session.delete(:login_uid)
+    redirect_to action: :main
+  end
 end
